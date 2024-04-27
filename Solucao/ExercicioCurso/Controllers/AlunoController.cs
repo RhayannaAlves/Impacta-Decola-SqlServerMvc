7f0fca5143a878437ef14fd6062f4194acbd0c51
Rhayanna Paz da Silva Alves <r.paz.da.silva.alves@avanade.com>
2024-04-26 21:03:50 -0300

    carga

---
+++ Solucao/ExercicioCurso/Controllers/AlunoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleCurso.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ Solucao/ExercicioCurso/Controllers/CursoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleCurso.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
